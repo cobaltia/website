aa042ff501d5f3c88eec4c9c0b5cf9f53fa2fdc1
JuanPablo2655 <personal@isidrocasiano.com>
2025-05-25 18:16:29 -0400

    fix: cleaner nav items look

--- src/app/(home)/items/page.tsx
+++ src/app/(home)/items/page.tsx
@@ -1,7 +1,16 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Items</h1>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center gap-3 pb-3">
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+          Items
+        </h2>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-center">
+          This page is under construction. Please check back later.
+        </p>
+      </div>
+    </>
   );
 }
